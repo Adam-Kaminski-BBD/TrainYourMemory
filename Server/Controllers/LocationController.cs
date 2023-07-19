e7aa3ffc09541404e49a10a088bfd055f70ffca8
Daniel Trevor Harding <danielha@bbd.co.za>
2023-07-19 21:25:38 +0200

    Adding Start of controllers

---
+++ Server/Controllers/LocationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LocationController : Controller
+    {
+        [HttpGet(Name = "")]
+        public User GetLocations()
+        {
+            return new User(1, "Daniel@gmail.com", "daniel");
+        }
+    }
+}
---
+++ Server/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Server.Models
+{
+    public class User
+    {
+
+        public User(int id, string? email, string? name)
+        {
+            Id = id;
+            Email = email;
+            Name = name;
+        }
+
+        private int Id {  get; set; }
+        private string? Email { get; set; }
+        private string? Name { get; set; }
+
+    }
+}
