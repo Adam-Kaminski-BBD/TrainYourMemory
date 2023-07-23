288c283c4ef8f730fe3368c5d31d6c162a2d3ba8
Daniel Trevor Harding <danielha@bbd.co.za>
2023-07-23 15:55:52 +0200

    Adding more controller rputes

--- Server/Models/Drink.cs
+++ Server/Models/Drink.cs
@@ -1,17 +1,23 @@
-﻿namespace Server.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Server.Models
 {
     public class Drink
     {
 
+
+        [JsonIgnore]
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
         public double AlcoholPercetnage { get; set; }
 
-        public Drink (string name, string type, double alcoholPercetnage)
+        public Drink (string name, string type, double alcoholPercetnage, int id)
         {
             Name = name;
             Type = type;
             AlcoholPercetnage = alcoholPercetnage;
+            Id = id;
         }
     }
 }
---
+++ Server/Models/Log.cs
@@ -0,0 +1,23 @@
+﻿namespace Server.Models
+{
+    public class Log
+    {
+
+        public string Location { get; set; }    
+        public string Drink { get; set; }   
+        public double Cost { get; set; }    
+        public int Quantity { get; set; }
+        public string Username { get; set; }
+        public DateTime Date { get; set; }  
+
+        public Log(string location, string drink, double cost, int quantity, string username, DateTime date)
+        {
+            Location = location;
+            Drink = drink;
+            Cost = cost;
+            Quantity = quantity;
+            Username = username;
+            Date = date;
+        }
+    }
+}
