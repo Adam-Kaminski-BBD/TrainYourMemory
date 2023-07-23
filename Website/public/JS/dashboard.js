d1b838434216c852b709838ec5567d95f602538c
Adam Kaminski <adamka@bbd.co.za>
2023-07-23 13:01:06 +0200

    Added consistentency across navbar and loading of navbar in js, can be refactored to insert in its own js script if need be

--- Website/public/JS/dashboard.js
+++ Website/public/JS/dashboard.js
@@ -1,17 +1,12 @@
 import * as Fn from './navbar.js';
 
-const navbarElement = Fn.renderNavbar(
-  '../Images/image1.png',
-  "John's Training",
-  [
-    { label: 'Friends', onclick: Fn.showFriends, active: true },
-    { label: 'Locations', onclick: Fn.showLocations },
-    { label: 'Drinks', onclick: Fn.showDrinks },
-    { label: 'History', onclick: Fn.showHistory },
-    { label: 'New Entry', onclick: Fn.showEntry },
-    { label: 'FaQ', onclick: Fn.showFaQ }
-  ]
-);
+document.addEventListener('DOMContentLoaded',()=>{
+  menuSetup();
+});
 
-const navbarContainer = document.getElementById('navbarContainer');
-navbarContainer.appendChild(navbarElement);
+function menuSetup(){
+  const navbarElement = Fn.renderNavbar();
+  const main = document.getElementsByTagName('main')[0];
+  const body = document.getElementsByTagName('body')[0];
+  body.insertBefore(navbarElement, main);
+}
