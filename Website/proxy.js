17fcafe3d63316995297ce34c7e50eb63a0ef890
Adam Kaminski <adamka@bbd.co.za>
2023-07-24 21:39:37 +0200

    Added basic proxy routes

--- Website/proxy.js
+++ Website/proxy.js
@@ -1,8 +1,45 @@
 const express = require('express');
 const route = express.Router();
 
+// Evaluate REST Level
+route.post('/entry', (req, res)=>{
+  
+});
+
+route.post('/user', (req, res)=>{
+  
+});
+
+route.post('/drinks:user', (req, res)=>{
+  
+});
+
+route.get('/drink/top:user', (req, res)=>{
+
+});
+
+route.post('/locations:user', (req, res)=>{
+  
+});
+
+route.post('/location/top:user', (req, res)=>{
+  
+});
+
+route.post('/spend:user', (req, res)=>{
+  
+});
+
+route.post('/history:user', (req, res)=>{
+  
+});
+
 route.post('/', (req, res)=>{
   
-})
+});
+
+route.post('/entry', (req, res)=>{
+  
+});
 
 module.exports = route;
