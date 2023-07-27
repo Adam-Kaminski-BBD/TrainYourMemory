c86699659e4f0b3d158f56ac4f4711b0069fcf1b
Adam Kaminski <adamka@bbd.co.za>
2023-07-27 17:23:07 +0200

    Added quiz name

--- Website/public/JS/quiz.js
+++ Website/public/JS/quiz.js
@@ -1,11 +1,14 @@
 import * as Fn from './navbar.js';
-const name = 'Thabang'; 
 
-document.addEventListener('DOMContentLoaded',()=>{
-  menuSetup();
+document.addEventListener('DOMContentLoaded',async ()=>{
+  const userCheck = await fetch('/api/user');
+  const userInfo = await userCheck.json();
+  
+  const name = userInfo.name;
+  menuSetup(name);
 });
 
-function menuSetup(){
+function menuSetup(name){
   const navbarElement = Fn.renderNavbar(name);
   const main = document.getElementsByTagName('main')[0];
   const body = document.getElementsByTagName('body')[0];
